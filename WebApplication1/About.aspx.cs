1daf5eb051b5a83a469e100c6b86081f93653964
Daniel Novoa <daniel.novoa@serti.com.mx>
2012-09-04 19:07:42 -0500

    ss

--- WebApplication1/About.aspx.cs
+++ WebApplication1/About.aspx.cs
@@ -8,9 +8,11 @@ using System.Web.UI.WebControls;
 namespace WebApplication1
 {
     public partial class About : System.Web.UI.Page
-    {
+    { 
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (1 == 1)
+                return;
 
         }
     }
